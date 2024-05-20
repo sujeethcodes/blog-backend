2f6f3dc1589a83066c5dc05a184d5acd5fe4de32
sujeethcodes <sujeethcodes@gmail.com>
2024-05-20 16:34:58 +0530

    createDB

--- api/server.js
+++ api/server.js
@@ -1,7 +1,9 @@
 require("dotenv").config()
+const Connection = require("./config/db")
 const express = require("express")
 const app = express()
 const PORT = 8000 ||  process.env.PORT
+Connection()
 app.listen(PORT,()=>{
     console.log(`This port is running in ${PORT}`)
 })
