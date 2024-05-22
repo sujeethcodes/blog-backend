3e03278cadb07f1ec826361b101ea9b403c859dc
sujeethcodes <sujeethcodes@gmail.com>
2024-05-22 14:04:57 +0530

    uploadchange

--- api/controller/post.js
+++ api/controller/post.js
@@ -9,16 +9,34 @@ controller.createPost = async (req, res) => {
         if (!findAccount) return res.status(406).json({ status: 406, message: "User not found" });
 
         let post = req?.file;
-        if (!post) return res.status(400).json({ status: 400, message: "No file uploaded" });
+        if (!post) return res.json({ status: 400, message: "No file uploaded" });
 
         const createPost = await Post.create({ userId: req.body?.userId, post: post.filename });
         if (!createPost) return res.status(406).json({ status: 406, message: "Post creation failed" });
 
-        return res.status(200).json({ status: 200, message: "Post created", filePath: post.path });
+        return res.json({ status: 200, message: "Post created", filePath: post.path });
     } catch (error) {
         console.error(error);
-        return res.status(500).json({ status: 500, message: "Internal server error" });
+        return res.json({ status: 500, message: "Internal server error" });
     }
 };
+
+controller.editPost = async(req, res)=>{
+    try {
+        let findAccount = await helperUtils.findAccount(req?.body?.userId);
+        if (!findAccount) return res.status(406).json({ status: 406, message: "User not found" });
+
+        let post = req?.file;
+        if (!post) return res.status(400).json({ status: 400, message: "No file uploaded" });
+
+        const updatePost = await Post.upload({ userId: findAccount?._id, _id : req?.body?.podtId}, {$set:{post: post.filename }});
+        if (!updatePost) return res.status(406).json({ status: 406, message: "Post creation failed" });
+
+        return res.json({ status: 200, message: "Post updated", filePath: post.path });
+        
+    } catch (error) {;
+        return res.json({ status: 500, message: "Internal server error" });
+    }
+}
 
 module.exports = controller;
