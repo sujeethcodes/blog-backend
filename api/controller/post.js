4ecd3fa4e9f9689d4d46022ea6503a19132d5f39
sujeethcodes <sujeethcodes@gmail.com>
2024-05-25 14:02:29 +0530

    codeStructure

--- api/controller/post.js
+++ api/controller/post.js
@@ -4,39 +4,61 @@ const helperUtils = require("../utils/helper");
 const controller = {};
 
 controller.createPost = async (req, res) => {
-    try {
-        let findAccount = await helperUtils.findAccount(req?.body?.userId);
-        if (!findAccount) return res.status(406).json({ status: 406, message: "User not found" });
+  try {
+    let findAccount = await helperUtils.findAccount(req?.body?.userId);
+    if (!findAccount)
+      return res.status(406).json({ status: 406, message: "User not found" });
 
-        let post = req?.file;
-        if (!post) return res.json({ status: 400, message: "No file uploaded" });
+    let post = req?.file;
+    if (!post) return res.json({ status: 400, message: "No file uploaded" });
 
-        const createPost = await Post.create({ userId: req.body?.userId, post: post.filename });
-        if (!createPost) return res.status(406).json({ status: 406, message: "Post creation failed" });
+    const createPost = await Post.create({
+      userId: req.body?.userId,
+      post: post.filename,
+    });
+    if (!createPost)
+      return res
+        .status(406)
+        .json({ status: 406, message: "Post creation failed" });
 
-        return res.json({ status: 200, message: "Post created", filePath: post.path });
-    } catch (error) {
-        console.error(error);
-        return res.json({ status: 500, message: "Internal server error" });
-    }
+    return res.json({
+      status: 200,
+      message: "Post created",
+      filePath: post.path,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.json({ status: 500, message: "Internal server error" });
+  }
 };
 
-controller.editPost = async(req, res)=>{
-    try {
-        let findAccount = await helperUtils.findAccount(req?.body?.userId);
-        if (!findAccount) return res.status(406).json({ status: 406, message: "User not found" });
+controller.editPost = async (req, res) => {
+  try {
+    let findAccount = await helperUtils.findAccount(req?.body?.userId);
+    if (!findAccount)
+      return res.status(406).json({ status: 406, message: "User not found" });
 
-        let post = req?.file;
-        if (!post) return res.status(400).json({ status: 400, message: "No file uploaded" });
+    let post = req?.file;
+    if (!post)
+      return res.status(400).json({ status: 400, message: "No file uploaded" });
 
-        const updatePost = await Post.upload({ userId: findAccount?._id, _id : req?.body?.podtId}, {$set:{post: post.filename }});
-        if (!updatePost) return res.status(406).json({ status: 406, message: "Post creation failed" });
+    const updatePost = await Post.upload(
+      { userId: findAccount?._id, _id: req?.body?.podtId },
+      { $set: { post: post.filename } }
+    );
+    if (!updatePost)
+      return res
+        .status(406)
+        .json({ status: 406, message: "Post creation failed" });
 
-        return res.json({ status: 200, message: "Post updated", filePath: post.path });
-        
-    } catch (error) {;
-        return res.json({ status: 500, message: "Internal server error" });
-    }
-}
+    return res.json({
+      status: 200,
+      message: "Post updated",
+      filePath: post.path,
+    });
+  } catch (error) {
+    return res.json({ status: 500, message: "Internal server error" });
+  }
+};
 
 module.exports = controller;
--- api/utils/upload.js
+++ api/utils/upload.js
@@ -1,13 +1,13 @@
-const multer = require('multer');
-const uuid = require('uuid');
-const uploadDir = 'uploads/';
+const multer = require("multer");
+const uuid = require("uuid");
+const uploadDir = "uploads/";
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, uploadDir);
-    },
-    filename: function (req, file, cb) {
-        cb(null, uuid.v4()+" "+ file.originalname);
-    }
+  destination: function (req, file, cb) {
+    cb(null, uploadDir);
+  },
+  filename: function (req, file, cb) {
+    cb(null, uuid.v4() + " " + file.originalname);
+  },
 });
 
 const upload = multer({ storage: storage });
