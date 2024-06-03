2d4431b2c58b95eb012e7db9e30d98713af25419
sujeethcodes <sujeeth.s@digamend.com>
2024-06-03 12:31:08 +0530

    editPost

--- api/controller/post.js
+++ api/controller/post.js
@@ -34,16 +34,16 @@ controller.createPost = async (req, res) => {
 
 controller.editPost = async (req, res) => {
   try {
-    let findAccount = await helperUtils.findAccount(req?.body?.userId);
-    if (!findAccount)
-      return res.status(406).json({ status: 406, message: "User not found" });
-
     let post = req?.file;
     if (!post)
       return res.status(400).json({ status: 400, message: "No file uploaded" });
 
-    const updatePost = await Post.upload(
-      { userId: findAccount?._id, _id: req?.body?.podtId },
+    let findAccount = await helperUtils.findAccount(req?.body?.userId);
+    if (!findAccount)
+      return res.status(406).json({ status: 406, message: "User not found" });
+
+    const updatePost = await Post.updateOne(
+      { userId: findAccount?._id, _id: req?.body?.postId },
       { $set: { post: post.filename } }
     );
     if (!updatePost)
@@ -56,8 +56,8 @@ controller.editPost = async (req, res) => {
       message: "Post updated",
       filePath: post.path,
     });
-  } catch (error) {
-    return res.json({ status: 500, message: "Internal server error" });
+  } catch (e) {
+    return res.json({ status: 500, message: e.message });
   }
 };
 
--- api/routes/post.js
+++ api/routes/post.js
@@ -4,5 +4,6 @@ const postController = require("../controller/post");
 const upload = require("../utils/upload");
 
 routes.post("/createPost", upload.single('post'), postController.createPost);
+routes.put("/editPost", upload.single('post'), postController.editPost)
 
 module.exports = routes;
